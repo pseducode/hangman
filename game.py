59e6dadea61eac313a6bbdea2a11b9e896a16071
pseducode <10664638+pseducode@users.noreply.github.com>
2022-09-20 18:44:32 -0400

    game now picks a solution from inputtedlist

--- game.py
+++ game.py
@@ -1,7 +1,10 @@
 import hanging
 def main():
     print("Hello Mr. Hangman player!")
+    game = hanging.hanging("medium")
+    game.picksolution()
     
 if __name__=="__main__":
     main()
-    game = hanging.hanging("easy")
+    
+   
--- hanging.py
+++ hanging.py
@@ -1,5 +1,5 @@
+import random
 class hanging:
-    import random
     #class to keep track of hangman state (letters guessed, game solution,letters guessed so far and # of wrong guessed)
     #Global variables
     mode=""
@@ -10,18 +10,23 @@ class hanging:
     def __init__(self,hardness):
         self.mode =hardness #hardness can be easy, medium or hard, use external .txt wordlists
         #pick random solution, from inuputted difficulty setting
-        picksolution()
+        print("deciding on a solution, hold on a minute")
+        #if (mode=="easy"):
+            #wordchoices
+        #picksolution()
         
     def gameover(self):
         #True/False as to wether max guesses have been reached, or solution
         return false
 
-    def picksolution():
+    def picksolution(self):
         #At the astart of the game, pick the solution, based on the difficulty inputted at random, from appropriate wordlist
         print("deciding on a solution, hold on")
         #first determine wordlist to use
-        if(mode=="easy"):
+        if(self.mode=="easy"):
             wordchoices = open("easy.txt","r")
+        elif (self.mode=="medium"):
+            wordchoices = open("medium.txt","r")    
         else:
             wordchoices= open("hard.txt","r")
         #next determine length of wordlist and pick one of the solutions
