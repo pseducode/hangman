57c4f55c061d81157eee4c1ea958ac43a279d32c
pseducode <10664638+pseducode@users.noreply.github.com>
2022-11-12 18:35:05 -0500

    minimu,m viable game works in failking case, but not in solved case for some reason

--- board2.py
+++ board2.py
@@ -4,30 +4,42 @@ class board2:
     numwrong=0 #number of incorrect guesses a player has made
     guesses=[] #all guesses made so far
     wrongguess=[] #incorrect guesses
-    nonlets=('.',',','?',',','!')#non-letter characters in solution, possible
+    nonlets=('.',',','?',',', ' ','!')#non-letter characters in solution, possible
     won=False #guessed all letters in solutyion or not
 
     def __init__(self,answer):
-        self.solution=answer
+        self.solution=answer[:-1]
         self.guesses=[]
         self.numwrong=0
         self.wrongguess=[]
 
-    def makeguess(guess):
+    def makeguess(self, guess):
         self.guesses.append(guess.lower())
         if guess.lower() in self.solution:
             return True
         else:
             self.numwrong+=1
+            #self.wrongguess.append(guess)
             return False
-    def howmanywrong():
+    def howmanywrong(self):
         #return number of wrong guesses made
         return self.numwrong
     def wrongletters():
         #return list of all wrongly guessed letters
         return self.wrongguess
-    def boardstatus(): #returmn solution so far
-        pass
+    def boardstatus(self): #returmn solution so far
+        todraw = ""
+        for letter in self.solution:
+            if((letter.lower() not in self.guesses) and (letter not in self.nonlets)):
+                todraw+="_ "
+            elif letter in self.nonlets:
+                todraw+=letter
+            else:
+                todraw+=letter
+        return todraw
+    
+        
+        
         
 
 
--- minviablegame.py
+++ minviablegame.py
@@ -7,14 +7,47 @@ import hanging
 import board2
 import random
 options =["easy","medium","hard"]
-allowedwrong=0
+allowedwrong=0 #number of allowed incorrect guesses before gameover, max =  7 for easy mode (head,body,L-arm,R-arm,R-leg,L-leg, and hat),
+#Medium & hard modes only get 6 (not hat)
 
+def playgame(sol):
+    #main game loop outside of main, for debugging purposes, solution is solution to game
+    gameover=False
+    b=board2.board2(sol)
+    while not gameover:
+         print("The boardlooks like this currently: ")
+         bstate = b.boardstatus()
+         print(bstate)
+         print("to debug, bstate is: "+bstate+" and the sol is: "+sol)
+         print("Please enter a guess for letter in solution: ")
+         totry=input()
+         state = b.makeguess(totry)
+         if state:
+             print("you made a correct guess!")
+         else:
+              print("you made an incorrect guess!")
+         if (bstate==sol):
+              print("Congratulations, you have won the game!")
+              gameover=True
+         elif(b.howmanywrong() >= allowedwrong):
+            print("Sorry, you've had to many incorrect guesseses. Game Over!")
+            print("FYI, the solution was: "+sol)
+            gameover=True
+         
+        
+        
 def picksolution(mode):
+    global allowedwrong
     print("selecting a solution")
     fname = mode + ".txt" #file name of solution list to open
     wordchoices = open(fname, "r")
     wordlist = wordchoices.readlines()
     numoptions =len(wordlist)
+    #determing number of allowed incorrectguesses based on doifficulty selecxted.
+    if mode =="easy":
+        allowedwrong=7
+    else:
+        allowedwrong=6
     return(wordlist[random.randint(0,numoptions-1)])
 def main2():
     pygame.init()
@@ -32,6 +65,8 @@ def main2():
             print("Alrighty, you've selected the difficulty of: "+level)
     solution = picksolution(level)
     print("alrighty then, given your chosen difficulty, the solution I've picked is:\n"+solution)
+    print ("The number of possible wrong guiesses you havem given difficulty selected is: "+str(allowedwrong))
+    playgame(solution)
     
 
 if __name__=="__main__":
